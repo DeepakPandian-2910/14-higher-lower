9ddf9a2ffa749451b924b225d2d95ae51b39da29
DeepakPandian-2910 <88448308+DeepakPandian-2910@users.noreply.github.com>
2021-12-06 02:26:50 +0530

    Create 14) higher lower.py

---
+++ 14) higher lower.py
@@ -0,0 +1,131 @@
+import random
+data = [
+    {
+        'name': 'Instagram',
+        'follower_count': 346,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 215,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Ariana Grande',
+        'follower_count': 183,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'follower_count': 181,
+        'description': 'Actor and professional wrestler',
+        'country': 'United States'
+    },
+    {
+        'name': 'Selena Gomez',
+        'follower_count': 174,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'follower_count': 172,
+        'description': 'Reality TV personality and businesswoman and Self-Made Billionaire',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'follower_count': 167,
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count': 149,
+        'description': 'Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Beyoncé',
+        'follower_count': 145,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Neymar',
+        'follower_count': 138,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'National Geographic',
+        'follower_count': 135,
+        'description': 'Magazine',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Bieber',
+        'follower_count': 133,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Taylor Swift',
+        'follower_count': 131,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kendall Jenner',
+        'follower_count': 127,
+        'description': 'Reality TV personality and Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Jennifer Lopez',
+        'follower_count': 119,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Nicki Minaj',
+        'follower_count': 113,
+        'description': 'Musician',
+        'country': 'Trinidad and Tobago'
+    }
+]
+print("Welcome to higher or lower!")
+score = 0
+def format_data(account):
+  account_name = account["name"]
+  account_description = account["description"]
+  account_country = account["country"]
+  return f"{account_name}, {account_description}, {account_country}"
+def check(guess, A_follower, B_follower):
+  if A_follower > B_follower:
+    return guess == "A"
+  else:
+    return guess == "B"
+B = random.choice(data)
+game_over = False
+while not game_over:
+  A = B
+  B = random.choice(data)
+  if B == A:
+    B = random.choice(data)
+  print(f"Compare A: {format_data(A)}.")
+  print("vs")
+  print(f"Compare B: {format_data(B)}.")
+  guess = input("Who has more followers? Type 'A' or 'B': ").upper()
+  A_follower = A["follower_count"]
+  B_follower = B["follower_count"]
+  correct = check(guess, A_follower, B_follower)
+  if correct:
+    score += 1
+    print(f"You're right! Current score: {score}.")
+  else:
+    print(f"Sorry, thats wrong. Final score: {score}.")
+    game_over = True
